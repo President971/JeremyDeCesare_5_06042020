6e280512d63b7957d9fdb35e21e6bbae68562765
DE CESARE <decesare971@gmail.com>
2021-07-30 10:48:59 +0200

    v1.4

--- front/index.js
+++ front/index.js
@@ -1,5 +1,19 @@
-fetch("http://localhost:3000/api/teddies")
-.then(response => response.json())
-.then(response => alert(JSON.stringify(response)))
-;
+main();
 
+function main() {
+  getArticles();
+}
+
+// Récupérer les articles depuis l'API
+function getArticles() {
+  fetch("http://localhost:3000/api/teddies")
+    .then(function (res) {
+      return res.json();
+    })
+    .catch((error) => {
+      let productsContainer = document.querySelector(".products-container");
+      productsContainer.innerHTML =
+        "Nous n'avons pas réussi à afficher nos nounours. Avez vous bien lancé le serveur local (Port 3000) ? <br>Si le problème persiste, contactez-nous.";
+      productsContainer.style.textAlign = "center";
+      productsContainer.style.padding = "30vh 0";
+    })
