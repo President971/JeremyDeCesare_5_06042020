e6b350be930def1a1e6ad7b3067dbbf5b0ec34a2
DE CESARE <decesare971@gmail.com>
2021-07-30 13:01:43 +0200

    v1.4.1

--- front/index.js
+++ front/index.js
@@ -11,9 +11,9 @@ function getArticles() {
       return res.json();
     })
     .catch((error) => {
-      let productsContainer = document.querySelector(".products-container");
+      let productsContainer = document.querySelector(".py-5");
       productsContainer.innerHTML =
         "Nous n'avons pas réussi à afficher nos nounours. Avez vous bien lancé le serveur local (Port 3000) ? <br>Si le problème persiste, contactez-nous.";
       productsContainer.style.textAlign = "center";
       productsContainer.style.padding = "30vh 0";
-    })
+    })}
