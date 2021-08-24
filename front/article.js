75478ca265f869f7cb9535f1b39ba8fbf0d84eea
DECESARE <decesare971@gmail.com>
2021-08-24 18:50:25 +0200

    Mise en place bouton produit

--- front/article.js
+++ front/article.js
@@ -23,4 +23,14 @@ function hydrateArticle(article){
  document.getElementById("cardImg").src = article.imageUrl;
  document.getElementById("cardTitle").textContent = article.name
  document.getElementById("cardBody").textContent = article.description
+ document.getElementById("cardPrice").textContent = article.price /100 + " €"
+ document.getElementById('productColor').style.gridTemplateRows = `repeat(${article.colors.length}, 1fr)`
+
+ let colorSelect = document.getElementById("productColor");
+ for (let i = 0; i < article.colors.length; i++) {
+   let option = document.createElement("option");
+   option.innerText = article.colors[i];
+   colorSelect.appendChild(option);
 }
+}
+
