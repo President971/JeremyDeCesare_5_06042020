149efc833ed5412ec5d7818a011a1b7282934de3
DECESARE <decesare971@gmail.com>
2021-08-31 19:10:37 +0200

    v1.62

--- front/cart.js
+++ front/cart.js
@@ -4,10 +4,14 @@ console.log(produitLocalStorage);
 //-------Affichage des produits du Panier----
 //------Endroit ou j'inject le HTML
 const positionProduit = document.querySelector("#tableProduit")
+const positionTable = document.querySelector("#tableau")
+
 // Si Panier est vide 
 if(produitLocalStorage === null) {
     const panierVide =`<h2>Votre panier est vide. <a href="index.html">Remplissez le d'abord !</a></h2>`;
     positionProduit.innerHTML = panierVide;
+    let Table = document.getElementById("tableau");
+    Table.remove()
 } else {
     ///si le panier est pas vide
     let produitPanier =[];
@@ -19,8 +23,7 @@ if(produitLocalStorage === null) {
         <td>${produitLocalStorage[j].price}</td>
       </tr>
       `;
+    }
       if(j == produitLocalStorage.length)
       positionProduit.innerHTML = produitPanier;
-    }
-
 }
